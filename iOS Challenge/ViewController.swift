3c76e8343aa960e8496718e7aa8776626a16d936
lagaren <lagaren@gmail.com>
2018-10-02 16:03:07 -0400

    First commit for the Clutch Technologies iOS coding challenge

--- iOS Challenge/ViewController.swift
+++ iOS Challenge/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var helloWorldButton: UIButton?
+    @IBOutlet weak var vanishButton: UIButton?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
+    @IBAction func showMessage(){
+        //Hello world code from tutorial video
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World",preferredStyle:UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler:nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func hideHelloWorld(__sender: UIButton){
+        //Checks existence of buttons and unwraps them via binding
+        if let helloWorldButton = helloWorldButton, let vanishButton = vanishButton {
+            //Hides/reveals the red button
+            helloWorldButton.isHidden = !(helloWorldButton.isHidden)
+            
+            //Changes label of the green button
+            if (helloWorldButton.isHidden) {
+                vanishButton.setTitle("Now you don't", for: [])
+            } else {
+                vanishButton.setTitle("Now you see him", for: [])
+            }
+        }
+    }
 
 }
 
